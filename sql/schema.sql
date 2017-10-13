00943bef7dca68e8194aae7ab62da5ac42f8687d
Austin Dudas <dudasaus@umich.edu>
2017-10-13 18:01:10 -0400

    Modified schema for postgres

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,76 +1,84 @@
-# Planning on using MySQL
-DROP DATABASE IF EXISTS olympics;
-CREATE DATABASE olympics;
+--- Planning on using PostgreSQL
+-- DROP DATABASE IF EXISTS olympics;
+-- CREATE DATABASE olympics;
 
-USE olympics;
+-- \c olympics;
 
-### SECTION 1: TABLES FOR INFORMATION ABOUT THE ACTUAL OLYMPICS ###
+--- SECTION 1: TABLES FOR INFORMATION ABOUT THE ACTUAL OLYMPICS ---
 
-# General category (e.g. skiing)
+--- General category (e.g. skiing)
+DROP TABLE IF EXISTS sports_category;
 CREATE TABLE sports_category (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     name VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 );
 
-# Specific type of event (e.g. Mens halfpipe)
+--- Specific type of event (e.g. Mens halfpipe)
+DROP TABLE IF EXISTS sports_events;
 CREATE TABLE sports_events (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     name VARCHAR(40) NOT NULL,
     category_id INTEGER NOT NULL REFERENCES sports_category(id),
-    team_event BOOLEAN NOT NULL DEFAULT 0,
+    team_event BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
-# Actual event with times (e.g. Mens halfpipe qualifiers)
+--- Actual event with times (e.g. Mens halfpipe qualifiers)
+DROP TABLE IF EXISTS events;
 CREATE TABLE events (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     name VARCHAR(120) NOT NULL,
     event_id INTEGER NOT NULL REFERENCES sports_events(id),
-    time DATETIME NOT NULL,
+    time TIMESTAMP NOT NULL,
     finals BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
-# Country info
+--- Country info
+DROP TABLE IF EXISTS countries;
 CREATE TABLE countries (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     name VARCHAR(40) NOT NULL,
     short_name VARCHAR(3),
     PRIMARY KEY (id)
 );
 
-# Athletes info
+--- Athletes info
+DROP TABLE IF EXISTS athletes;
 CREATE TABLE athletes (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     name VARCHAR(40) NOT NULL,
     country_id INTEGER NOT NULL REFERENCES countries(id),
     PRIMARY KEY (id)
 );
 
-# Connections between athletes and their sports event(s)
+--- Connections between athletes and their sports event(s)
+DROP TABLE IF EXISTS athletes_sports_events;
 CREATE TABLE athletes_sports_events (
     athlete_id INTEGER NOT NULL REFERENCES athletes(id),
     sports_event_id INTEGER NOT NULL REFERENCES sports_events(id),
     PRIMARY KEY (athlete_id, sports_event_id)
 );
 
-# Info about which countries participate in which sports events
+--- Info about which countries participate in which sports events
+DROP TABLE IF EXISTS countries_sports_events;
 CREATE TABLE countries_sports_events (
     country_id INTEGER NOT NULL REFERENCES countries(id),
     sports_event_id INTEGER NOT NULL REFERENCES sports_events(id),
     PRIMARY KEY (country_id, sports_event_id)
 );
 
-# Results info for events
+--- Results info for events
+DROP TABLE IF EXISTS results;
 CREATE TABLE results (
     sports_event_id INTEGER NOT NULL REFERENCES sports_events(id),
     country_id INTEGER NOT NULL REFERENCES countries(id),
-    athlete_id INTEGER REFERENCES athletes(id), # Can be null if team event
-    medal INTEGER NOT NULL, # 1 = bronze, 2 = silver, 3 = gold?
+    athlete_id INTEGER REFERENCES athletes(id), --- Can be null if team event
+    medal INTEGER NOT NULL, --- 1 = bronze, 2 = silver, 3 = gold?
     PRIMARY KEY (sports_event_id)
 );
 
 
 
-### SECTION 2: TABLES ABOUT USERS, GROUPS, PREDICTIONS, SCORING, ETC. ###
+--- SECTION 2: TABLES ABOUT USERS, GROUPS, PREDICTIONS, SCORING, ETC. ---
