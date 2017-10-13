a8d4245aa49417acb12178d50865d27a9e309a29
Austin Dudas <dudasaus@umich.edu>
2017-10-13 18:47:36 -0400

    connecting to db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { Client } = require('pg');
 const app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
 // TODO: Make another function to handle API calls?
 app.get('/api/*', (req, res) => {
+    dbtest();
     res.send(`<pre>${JSON.stringify({'data': 0}, null, 2)}</pre>`);
 });
 
@@ -17,3 +19,22 @@ app.get('/*', (req, res) => {
 app.listen(app.get('port'), () => {
     process.stdout.write(`Listening on port ${app.get('port')}\n`);
 });
+
+function dbtest() {
+    console.log(process.env.DATABASE_URL);
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    });
+
+    client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    });
+}
