40ab144ebb0ff5827870be3d2460c63b1674bf87
Austin Dudas <dudasaus@umich.edu>
2017-10-30 13:53:15 -0400

    Started category delete - untested

--- api/controllers/sportsCategoriesController.js
+++ api/controllers/sportsCategoriesController.js
@@ -47,7 +47,51 @@ function addCategory(req, res) {
   }
   else {
     // Expected body values don't exist
-    errorResponse(res, 404, 'Missing expected body data.');
+    errorResponse(res, 404, 'Missing expected body data');
   }
 }
 module.exports.addCategory = addCategory;
+
+// Delete categories
+function deleteCategory(req, res) {
+  const values = [req.body.id];
+
+  // Check for write permissions
+  if (!writePermission(req)) {
+    errorResponse(res, 403, 'Inavlid or missing API key');
+  }
+  // Check for expected body data
+  else if (noNullValues(values)) {
+    // Create client
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    });
+
+    // Read ID
+    let id = values[0];
+    if (id === 'all') {
+      // delete all categories
+      const sql = 'DELETE FROM sports_category RETURNING *';
+      client.query(sql, (err, cres) => {
+        if (err) throw err;
+        client.end();
+        // Return the deleted rows
+        res.json(cres.rows);
+      });
+    }
+    else if (!Number.isNaN(parseInt(id, 10))) {
+      id = parseInt(id, 10);
+      // TODO: delete category with id
+    }
+    else {
+      // Invalid id parameter
+      errorResponse(res, 404, 'Invalid id parameter');
+    }
+  }
+  else {
+    // eexpected body values don't exist
+    errorResponse(res, 404, 'Missing expected body data');
+  }
+}
+module.exports.deleteCategory = deleteCategory;
