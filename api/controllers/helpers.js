e68e2c897af14953668bb36161f81b7635b6205e
Austin Dudas <dudasaus@umich.edu>
2017-10-30 13:23:01 -0400

    added hashing of api keys

--- api/controllers/helpers.js
+++ api/controllers/helpers.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
 // Returns false if any values in the array are null or undefined
 function noNullValues(arr) {
@@ -20,10 +21,18 @@ function errorResponse(res, statusCode, msg) {
 }
 module.exports.errorResponse = errorResponse;
 
+// Returns a sha256 hash (unsalted) of msg
+function hash(msg) {
+  const hasher = crypto.createHash('sha256');
+  hasher.update(msg);
+  return hasher.digest('hex');
+}
+module.exports.hash = hash;
+
 // Check if a request has write permissions
 const apiKeys = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'apiKeys.json')));
 function writePermission(req) {
   const { key } = req.query;
-  return (key && apiKeys[key]);
+  return (key && apiKeys[hash(key)]);
 }
 module.exports.writePermission = writePermission;
